0ce7f8822fd00ad80f3393a92fdcc60e033f6519
Thomas Farley <ThomasFarley3@cmail.carleton.ca>
2021-03-04 01:21:46 -0500

    Added a home page that links to the hello world and created the static + templates resources folder -Thomas

---
+++ src/main/java/com.COMP3004.CMS/Display.java
@@ -0,0 +1,16 @@
+package com.COMP3004.CMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Display {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Display.class, args);
+    }
+
+
+
+}

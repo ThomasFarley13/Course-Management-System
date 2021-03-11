36dc3b841d55f53c7ec4c4effa8d804339346c31
seslafcb <56165989+seslafcb@users.noreply.github.com>
2021-03-11 12:37:38 -0500

    added login page and dashboard template

--- src/main/java/com.COMP3004.CMS/CourseManagementSystem.java
+++ src/main/java/com.COMP3004.CMS/CourseManagementSystem.java
@@ -1,18 +1,102 @@
 package com.COMP3004.CMS;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class CourseManagementSystem {
+public class CourseManagementSystem  {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    boolean logged_in = false;
+    String userHash;
+
+
+    @GetMapping("/")
+    public String home () {
+        if (!logged_in) {
+            return "forward:/login.html";
+        }
+        else {
+            return "you are logged in";
+        }
+    }
+
+    /*@RequestMapping(value = "/login", method = RequestMethod.POST)
+    public  ModelAndView login_handler(@RequestBody LoginForm loginForm, ModelMap model) {
+
+        //your code
+
+        // fake authenitcation
+        if (loginForm.getUsername().equals("Seppy") && loginForm.getPassword().equals( "IH83004")) {
+            System.out.println("Recived Valid Usernaame and password");
+
+            return new ModelAndView("redirect:/hello", model);
+        }
+        else {
+            // figure out message to send on fail and how to send it to the html
+
+        }
+        return null;
+
+    }*/
+
+    @PostMapping("/login")
+    public @ResponseBody ModelAndView loginhandler (@RequestBody LoginForm loginForm, ModelMap m) {
+
+        // fake authenitcation
+        if (loginForm.getUsername().equals("Seppy") && loginForm.getPassword().equals( "IH83004")) {
+            return  new ModelAndView("redirect:/hello",m);
+        }
+        else {
+            // figure out message to send on fail and how to send it to the html
+
+        }
+        return new ModelAndView("redirect:/login.html",m);
+    }
+
+    @GetMapping(value = "/hello")
+    public ModelAndView hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        System.out.println("We are in the Hello mapping function");
+        System.out.println(new ModelAndView("hello").toString());
+        return new ModelAndView("hello");
+    }
+
+    @GetMapping(value="/dashboardTest")
+    public String dashboardTest(Model model) {
+        System.out.println("We are in the DashboardTest mapping function");
+        String [] coursenames = {"Comp3000","Comp3001","Comp3002","Comp3003","Comp3004"};
+        String [] courselinks = {"courses/Comp3000","courses/Comp3001","courses/Comp3002","courses/Comp3003","courses/Comp3004"};
+
+        model.addAttribute("courses", coursenames);
+        model.addAttribute("links", courselinks);
+
+        return "dashboard";
+    }
+
+
+
+    static class LoginForm {
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public LoginForm(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
 
 }

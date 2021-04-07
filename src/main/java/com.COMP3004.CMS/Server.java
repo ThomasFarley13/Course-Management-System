8b96ef088f1b9a2ea7f1c9e89e4e81e4e8ec0424
Anita Chau <you@example.com>
2021-04-07 18:27:24 -0400

    updated mock courses and added a test professor

--- src/main/java/com.COMP3004.CMS/Server.java
+++ src/main/java/com.COMP3004.CMS/Server.java
@@ -44,6 +44,7 @@ public class Server implements CommandLineRunner {
         User.Student sepehr = (User.Student) factory.createUser("Sepehr", "Password423","Student", 4, "1994-12-25","male","Sepehr", "Eslami Amirabadi");
         User.Student thomas = (User.Student) factory.createUser("Thomas", "Password523","Student", 5,"2000-09-30", "other","Thomas", "Farley");
         User.Professor professor1 = (User.Professor) factory.createUser("Professor1", "Password623","Professor", 6,"null","null", "Big", "Sean");
+        User.Professor testProfessor = (User.Professor) factory.createUser("TestProfessor", "Password723", "Professor", 7, "null", "null", "Testing", "Tester");
         // user test data
 
         thomas.grading("3004B","B");
@@ -55,6 +56,7 @@ public class Server implements CommandLineRunner {
         abdul.setActive(true);
         thomas.setActive(true);
         professor1.setActive(true);
+        testProfessor.setActive(true);
 
 
         repository.save(admin);
@@ -62,53 +64,59 @@ public class Server implements CommandLineRunner {
         repository.save(anita);
         repository.save(sepehr);
         repository.save(thomas);
+        repository.save(professor1);
+        repository.save(testProfessor);
 
 
 
         //course test Data
 
         Courserepository.save(new Course("Object Oriented Software Programming","3004B",3000,3004,"Computer Science"));
+        handler.assign_prof(professor1.getUsername(), "3004B");
         Courserepository.save(new Course("The origin of Planets","2419B",2000,2419,"Earth Science"));
         Courserepository.save(new Course("Pro Basket Weaving", "1002D", 1000, 1002, "Liberal Arts"));
         Courserepository.save(new Course("Programming Paradigms","COMP3007B",3000,3007,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP3007B");
         Courserepository.save(new Course("Applied Cryptography","COMP4109A",4000,4109,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP4109A");
         Courserepository.save(new Course("Fundamentals Web Applications","COMP2406A",2000,2406,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP2406A");
         Courserepository.save(new Course("Operating Systems","COMP3000B",3000,3000,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP3000B");
         Courserepository.save(new Course("Design & Analysis of Algorithms","COMP3804A",3000,3804,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP3804A");
         Courserepository.save(new Course("Intro to Systems Programming","COMP2401A",2000,2401,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP2401A");
         Courserepository.save(new Course("Intro to Software Engineering","COMP2404A",2000,2404,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP2404A");
         Courserepository.save(new Course("Abstract Data Types/Algorithms","COMP2402B",2000,2402,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP2402B");
         Courserepository.save(new Course("Linear Algebra II","MATH2107A",2000,2107,"Math"));
+        handler.assign_prof(testProfessor.getUsername(), "MATH2107A");
         Courserepository.save(new Course("Intro to Computer Science II","COMP1406B",1000,1406,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP1406B");
         Courserepository.save(new Course("Intro to Computer Science I","COMP1405B",1000,1405,"Computer Science"));
+        handler.assign_prof("Professor1", "COMP1405B");
         Courserepository.save(new Course("Linear Algebra I","MATH1107A",1000,1107,"Math"));
+        handler.assign_prof(testProfessor.getUsername(), "MATH1107A");
         Courserepository.save(new Course("Discrete Structures I","COMP1805A",1000,1805,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP1805A");
         Courserepository.save(new Course("Discrete Structures II","COMP2804A",2000,2804,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP2804A");
         Courserepository.save(new Course("Introduction to Ecology","BIOL2600A",2000,2600,"Biology"));
+        handler.assign_prof(testProfessor.getUsername(), "BIOL2600A");
         Courserepository.save(new Course("Mysteries of the Mind","CGSC1001B",1000,1001,"Cognitive Science"));
+        handler.assign_prof(testProfessor.getUsername(), "CGSC1001B");
         Courserepository.save(new Course("Co-op","COOP1000D",1000,1000,"Co-op"));
         Courserepository.save(new Course("Introduction to Ecology","BIOL2600A",2000,2600,"Biology"));
+        handler.assign_prof(testProfessor.getUsername(), "BIOL2600A");
         Courserepository.save(new Course("Evolutionary Concepts","BIOL3609A",3000,3609,"Biology"));
+        handler.assign_prof(testProfessor.getUsername(), "BIOL3609A");
         Courserepository.save(new Course("General Biochemistry I","BIOC3101A",3000,3101,"Biochemistry"));
+        handler.assign_prof(testProfessor.getUsername(), "BIOC3101A");
         Courserepository.save(new Course("Computational Systems Biology","COMP4308A",4000,4308,"Computer Science"));
+        handler.assign_prof(testProfessor.getUsername(), "COMP4308A");
 
-        ((User.Professor) professor1).assignCourse("3004B");
-        ((User.Professor) professor1).assignCourse("COMP1405B");
-
-//        handler.assign_prof("professor1", "3004B");
-//        handler.assign_prof("professor1", "COMP1405B");
-
-        repository.save(professor1);
-
-
-
-
-
-
-
-
-
-//        Assigning course(s) to test prof
 
 
 
